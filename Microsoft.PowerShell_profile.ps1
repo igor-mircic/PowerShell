5751ebc217a1b5d776a8ed79641a37488f098eb6
Igor <76088411+igor-mircic@users.noreply.github.com>
2022-11-06 01:03:18 +0100

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,23 @@
+# Autocompletion #######################################################################################################
+
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Editor ###############################################################################################################
+
+$env:EDITOR = $env:VISUAL = 'nvim'
+Set-PsReadLineOption -EditMode Vi
+
+$OnViModeChange = [scriptblock]{
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    }
+    else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $OnViModeChange
+
+########################################################################################################################
